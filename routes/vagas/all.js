40952de93546d5774c6e2f3ed119029d65553ec1
ismpro <38300904+ismpro@users.noreply.github.com>
2023-01-30 20:25:39 +0000

    vagas all

--- routes/vagas/all.js
+++ routes/vagas/all.js
@@ -1,4 +1,5 @@
 const Vaga = require('../../models/Vaga');
+const User = require('../../models/User')
 
 /**
  * Route that return all roles
@@ -6,14 +7,38 @@ const Vaga = require('../../models/Vaga');
  * @returns {Vaga}
  */
 module.exports = function () {
-    return function (req, res) {
-        Vaga.find({}, function (err, vagas) {
-            if (!err) {
-                res.status(200).send(vagas)
+    return async function (req, res) {
+        if(req.session.userid) {
+            let user = await User.findById(req.session.userid);
+
+            if (user && user.atribuitesessionid === req.session.sessionId && user.type === "manager") {
+                Vaga.find({manager: user._id}, function (err, vagas) {
+                    if (!err) {
+                        res.status(200).send(vagas)
+                    } else {
+                        console.log(err)
+                        res.status(500).send(err)
+                    }
+                })
             } else {
-                console.log(err)
-                res.status(500).send(err)
+                Vaga.find({tipoVaga: true}, function (err, vagas) {
+                    if (!err) {
+                        res.status(200).send(vagas)
+                    } else {
+                        console.log(err)
+                        res.status(500).send(err)
+                    }
+                })
             }
-        })
+        } else {
+            Vaga.find({tipoVaga: false}, function (err, vagas) {
+                if (!err) {
+                    res.status(200).send(vagas)
+                } else {
+                    console.log(err)
+                    res.status(500).send(err)
+                }
+            })
+        }
     }
 }
